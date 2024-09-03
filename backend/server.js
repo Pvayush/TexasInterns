fa85b991582f3d94f373062e8449f5d6f39a7c7c
Pvayush <pvayush@gmail.com>
2024-09-03 00:41:49 -0500

    more things

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,6 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
